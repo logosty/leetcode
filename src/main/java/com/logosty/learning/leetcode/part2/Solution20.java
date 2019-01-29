ba1d9f9b7b7544096e8e198b157edb7da636fca2
logosty <614305527@qq.com>
2019-01-29 16:00:55 +0800

    完成 20. 有效的括号

--- src/main/java/com/logosty/learning/leetcode/part2/Solution20.java
+++ src/main/java/com/logosty/learning/leetcode/part2/Solution20.java
@@ -7,67 +7,87 @@ import java.util.*;
  * Date:2019/1/29 Time:15:16
  * Description: 20. 有效的括号
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
- *
+ * <p>
  * 有效字符串需满足：
- *
+ * <p>
  * 左括号必须用相同类型的右括号闭合。
  * 左括号必须以正确的顺序闭合。
  * 注意空字符串可被认为是有效字符串。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: "()"
  * 输出: true
  * 示例 2:
- *
+ * <p>
  * 输入: "()[]{}"
  * 输出: true
  * 示例 3:
- *
+ * <p>
  * 输入: "(]"
  * 输出: false
  * 示例 4:
- *
+ * <p>
  * 输入: "([)]"
  * 输出: false
  * 示例 5:
- *
+ * <p>
  * 输入: "{[]}"
  * 输出: true
  */
 public class Solution20 {
     public boolean isValid(String s) {
-        if (s == null || "".equals(s)) {
+        if (s == null || s.length() == 1) {
+            return false;
+        }
+        if (s.length() == 0) {
             return true;
         }
-        List<Character> list = new LinkedList<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '[' || c == '{' || c == '(') {
-                list.add(c);
-                continue;
-            }
-
-            if (list.size() == 0) {
-                return false;
-            }
-            Character removeC = list.remove(list.size() - 1);
-            if (c == ']' && removeC != '[') {
-                return false;
-            }
-            if (c == '}' && removeC != '{') {
-                return false;
-            }
-            if (c == ')' && removeC != '(') {
-                return false;
-            }
+        char[] arr = s.toCharArray();
+        if (arr[0] == ')' || arr[0] == ']' || arr[0] == '}') {
+            return false;
         }
 
-        return list.size() == 0;
+        int[] stack = new int[arr.length+1];
+        int pt = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int var = 0;
+            switch (arr[i]) {
+                case '{':
+                    var = 1;
+                    break;
+                case '}':
+                    var = -1;
+                    break;
+                case '[':
+                    var = 2;
+                    break;
+                case ']':
+                    var = -2;
+                    break;
+                case '(':
+                    var = 3;
+                    break;
+                case ')':
+                    var = -3;
+                    break;
+                default:
+                    break;
+            }
+
+            if (stack[pt] + var == 0) {
+                stack[pt] = 0;
+                pt--;
+            } else {
+                stack[++pt] = var;
+            }
+        }
+        return stack[1] == 0;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution20().isValid("]"));
+        System.out.println(new Solution20().isValid("()"));
     }
 }
