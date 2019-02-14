c07a6720f0435c12df4ca50b08abe63bce834f26
logosty <614305527@qq.com>
2019-02-14 14:45:22 +0800

    完成 简单 136. 只出现一次的数字

---
+++ src/main/java/com/logosty/learning/leetcode/part140/Solution136.java
@@ -0,0 +1,38 @@
+package com.logosty.learning.leetcode.part140;
+
+/**
+ * Created with IDEA by logosty
+ * Date:2019/2/14 Time:11:49
+ * Description: 简单 136. 只出现一次的数字
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ */
+public class Solution136 {
+    public int singleNumber(int[] nums) {
+        int i = 0;
+        for (int num : nums) {
+            i ^= num;
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2};
+        System.out.println(new Solution136().singleNumber(nums));
+    }
+}
