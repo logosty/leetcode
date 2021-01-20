1b677ca9bb975ef8d34c5bb7e135da3f317ac8c6
logosty <ganyingle@dayuwuxian.com>
2021-01-20 11:31:13 +0800

    finish: 628. 三个数的最大乘积 简单

--- src/main/java/com/logosty/learning/leetcode/section600/part62/Solution628.java
+++ src/main/java/com/logosty/learning/leetcode/section600/part62/Solution628.java
@@ -1,6 +1,7 @@
 package com.logosty.learning.leetcode.section600.part62;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * @author logosty(ganyingle) on 2021/1/20 10:52
@@ -34,7 +35,7 @@ public class Solution628 {
    * 22.85%
    * 的用户
    */
-  public int maximumProduct(int[] nums) {
+  public int maximumProduct2(int[] nums) {
     if (nums.length == 3) {
       return nums[0] * nums[1] * nums[2];
     }
@@ -43,6 +44,51 @@ public class Solution628 {
     int res = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
     res = Math.max(res, nums[0] * nums[1] * nums[nums.length - 1]);
     return res;
+  }
+
+  /**
+   * 执行用时：
+   * 2 ms
+   * , 在所有 Java 提交中击败了
+   * 99.13%
+   * 的用户
+   * 内存消耗：
+   * 40.1 MB
+   * , 在所有 Java 提交中击败了
+   * 16.82%
+   * 的用户
+   */
+  public int maximumProduct(int[] nums) {
+    if (nums.length == 3) {
+      return nums[0] * nums[1] * nums[2];
+    }
+
+    // 最小的和第二小的
+    int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+    // 最大的、第二大的和第三大的
+    int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+
+    for (int num : nums) {
+      if (num > max1) {
+        max3 = max2;
+        max2 = max1;
+        max1 = num;
+      } else if (num > max2) {
+        max3 = max2;
+        max2 = num;
+      } else if (num > max3) {
+        max3 = num;
+      }
+
+      if (num < min1) {
+        min2 = min1;
+        min1 = num;
+      } else if (num < min2) {
+        min2 = num;
+      }
+    }
+
+    return Math.max(min1 * min2 * max1, max1 * max2 * max3);
   }
 
   public static void main(String[] args) {
