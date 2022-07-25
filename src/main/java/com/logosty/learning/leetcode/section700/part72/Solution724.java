49fcb4bfda8ad20deabb891efef25d3fa531a304
ganyingle <ganyingle@kuaishou.com>
2022-07-25 16:39:04 +0800

    finish: 724. 寻找数组的中心索引 简单

--- src/main/java/com/logosty/learning/leetcode/section700/part72/Solution724.java
+++ src/main/java/com/logosty/learning/leetcode/section700/part72/Solution724.java
@@ -4,13 +4,13 @@ package com.logosty.learning.leetcode.section700.part72;
  * @author logosty(ganyingle) on 2020/5/27 10:53
  * 724. 寻找数组的中心索引 简单
  * 给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。
- *
+ * <p>
  * 我们是这样定义数组中心索引的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
- *
+ * <p>
  * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入:
  * nums = [1, 7, 3, 6, 5, 6]
  * 输出: 3
@@ -18,44 +18,42 @@ package com.logosty.learning.leetcode.section700.part72;
  * 索引3 (nums[3] = 6) 的左侧数之和(1 + 7 + 3 = 11)，与右侧数之和(5 + 6 = 11)相等。
  * 同时, 3 也是第一个符合要求的中心索引。
  * 示例 2:
- *
+ * <p>
  * 输入:
  * nums = [1, 2, 3]
  * 输出: -1
  * 解释:
  * 数组中不存在满足此条件的中心索引。
  * 说明:
- *
+ * <p>
  * nums 的长度范围为 [0, 10000]。
  * 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
  */
 public class Solution724 {
 
-  public int pivotIndex(int[] nums) {
-    if (nums.length == 0) {
-      return -1;
-    }
-    if (nums.length == 1) {
-      return 0;
+    public int pivotIndex(int[] nums) {
+        int left = 0;
+        int right = 0;
+        for (int i = 1; i < nums.length; i++) {
+            right += nums[i];
+        }
+        if (right == left) {
+            return 0;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            left += nums[i - 1];
+            right -= nums[i];
+            if (left == right) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
-    int sum = 0;
-    int left = 0;
-    for (int i = 0; i < nums.length; i++) {
-      sum = sum + nums[i];
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(new Solution724().pivotIndex(nums));
     }
-    for (int i = 0; i < nums.length; i++) {
-      if (left * 2 == sum - nums[i]) {
-        return i;
-      }
-      left = left + nums[i];
-    }
-
-    return -1;
-  }
-
-  public static void main(String[] args) {
-    int[] nums = {1,2,3};
-    System.out.println(new Solution724().pivotIndex(nums));
-  }
 }
