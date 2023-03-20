23e7bad84480455c4378e59f967e1d8206661648
ganyingle <ganyingle@kuaishou.com>
2023-03-20 16:30:20 +0800

    finish:3. 无重复字符的最长子串 中等

--- src/main/java/com/logosty/learning/leetcode/section000/part0/Solution3.java
+++ src/main/java/com/logosty/learning/leetcode/section000/part0/Solution3.java
@@ -6,7 +6,29 @@ import java.util.Map;
 /**
  * Created with IDEA by logosty
  * Date:2018/11/8 Time:20:16
- * Description: 给定一个字符串，找出不含有重复字符的最长子串的长度。
+ * Description: 3. 无重复字符的最长子串 中等
+ * 示例 1:
+ * <p>
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= s.length <= 5 * 104
+ * s 由英文字母、数字、符号和空格组成
  */
 public class Solution3 {
     public int lengthOfLongestSubstring(String s) {
@@ -30,11 +52,45 @@ public class Solution3 {
         return maxLenth;
     }
 
-
     public static void main(String[] args) {
-//        String s = "abcabcbb";
-//        String s = "pwwkew";
-        String s = "tmmzuxt";
-        System.out.println(new Solution3().lengthOfLongestSubstring(s));
+        String s = "aabaab!bb";
+        //        String s = "pwwkew";
+        //        String s = "tmmzuxt";
+        System.out.println(new Solution3().lengthOfLongestSubstring2(s));
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        if (s == null) {
+            return 0;
+        }
+        if (s.length() <= 1) {
+            return s.length();
+        }
+
+        Map<Character, Integer> cache = new HashMap<>();
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = 0;
+        int res = 1;
+        cache.put(chars[0], 0);
+
+        for (int i = 1; i < chars.length; i++) {
+            if (!cache.containsKey(chars[i])) {
+                cache.put(chars[i], i);
+                end++;
+                res = Math.max(res, end - start + 1);
+                continue;
+            }
+
+            int index = cache.get(chars[i]);
+            for (int j = start; j <= index; j++) {
+                cache.remove(chars[j]);
+            }
+            cache.put(chars[i], i);
+            start = index + 1;
+            end = i;
+        }
+
+        return res;
     }
 }
