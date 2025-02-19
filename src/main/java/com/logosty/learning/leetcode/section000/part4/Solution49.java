4f656d1f787e1ccf19e8d2713d4aff3eb42cecee
logosty <614305527@qq.com>
2025-02-19 16:26:34 +0800

    49. 字母异位词分组 中等

---
+++ src/main/java/com/logosty/learning/leetcode/section000/part4/Solution49.java
@@ -0,0 +1,52 @@
+package com.logosty.learning.leetcode.section000.part4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author logosty(ganyingle) on 2025/2/19 16:11
+ * description: 49. 字母异位词分组 中等
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * <p>
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 示例 2:
+ * <p>
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ * <p>
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ */
+public class Solution49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+            List<String> orDefault = map.getOrDefault(sorted, new ArrayList<>());
+            orDefault.add(str);
+            map.put(sorted, orDefault);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
