86ee657070acb06963bdd178de94f8bfbd6676ee
logosty <614305527@qq.com>
2025-05-09 17:02:30 +0800

    课程表 中等

--- src/main/java/com/logosty/learning/leetcode/section200/part20/Solution207.java
+++ src/main/java/com/logosty/learning/leetcode/section200/part20/Solution207.java
@@ -1,94 +1,78 @@
 package com.logosty.learning.leetcode.section200.part20;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author logosty(ganyingle) on 2021/1/19 15:19
  * 207. 课程表 中等
  * 你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
- *
+ * <p>
  * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
- *
+ * <p>
  * 给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: 2, [[1,0]]
  * 输出: true
  * 解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
  * 示例 2:
- *
+ * <p>
  * 输入: 2, [[1,0],[0,1]]
  * 输出: false
  * 解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。
  * 你可以假定输入的先决条件中没有重复的边。
  * 1 <= numCourses <= 10^5
  */
 public class Solution207 {
 
-  int[] visited;
-  List<Integer>[] lists;
-  boolean valid = true;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Set<Integer> allSet = new HashSet<>();
+        int[] indegrees = new int[numCourses];
+        Map<Integer, List<Integer>> graph = new HashMap<>();
 
-  public static void main(String[] args) {
-    System.out.println(new Solution207().canFinish(2, new int[][]{ {1, 0}}));
-  }
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int[] prerequisite : prerequisites) {
+            indegrees[prerequisite[1]]++;
+            List<Integer> list = graph.getOrDefault(prerequisite[0], new ArrayList<>());
+            list.add(prerequisite[1]);
+            graph.put(prerequisite[0], list);
 
-  /**
-   * 执行用时：
-   * 2 ms
-   * , 在所有 Java 提交中击败了
-   * 100.00%
-   * 的用户
-   * 内存消耗：
-   * 38.9 MB
-   * , 在所有 Java 提交中击败了
-   * 74.58%
-   * 的用户
-   * @param numCourses
-   * @param prerequisites
-   * @return
-   */
-  public boolean canFinish(int numCourses, int[][] prerequisites) {
-    lists = new List[numCourses];
-    visited = new int[numCourses];
-    for (int i = 0; i < lists.length; i++) {
-      lists[i] = new ArrayList<>();
-    }
+            allSet.add(prerequisite[0]);
+            allSet.add(prerequisite[1]);
+        }
+        for (Integer i : allSet) {
+            if (indegrees[i] == 0) {
+                stack.addLast(i);
+            }
+        }
 
-    for (int[] prerequisite : prerequisites) {
-      lists[prerequisite[1]].add(prerequisite[0]);
-    }
+        while (!stack.isEmpty()) {
+            int i = stack.pollFirst();
+            List<Integer> list = graph.getOrDefault(i, new ArrayList<>());
 
-    for (int i = 0; i < numCourses; i++) {
-      if (visited[i] == 0) {
-        dfs(i);
-      }
+            for (int linked : list) {
+                indegrees[linked]--;
+                if (indegrees[linked] == 0) {
+                    stack.addLast(linked);
+                }
+            }
+            allSet.remove(i);
+        }
+        return allSet.isEmpty();
     }
-    return valid;
-  }
-
-  private void dfs(int i) {
-    if (visited[i] == 1) {
-      return;
-    }
-    if (visited[i] == -1) {
-      valid = false;
-      return;
-    }
-    visited[i] = -1;
-    for (int j = 0; j < lists[i].size() && valid; j++) {
-      dfs(lists[i].get(j));
-    }
-    visited[i] = 1;
-  }
 
 }
