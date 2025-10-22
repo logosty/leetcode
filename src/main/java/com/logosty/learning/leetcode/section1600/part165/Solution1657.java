c346ccc20336343e35933ee5ba23a734e0f902fa
logosty <614305527@qq.com>
2025-10-22 16:55:29 +0800

    1657. 确定两个字符串是否接近 中等

---
+++ src/main/java/com/logosty/learning/leetcode/section1600/part165/Solution1657.java
@@ -0,0 +1,77 @@
+package com.logosty.learning.leetcode.section1600.part165;
+
+import java.util.Arrays;
+
+/**
+ * @author logosty(ganyingle) on 2025/10/21 18:45
+ * description: 1657. 确定两个字符串是否接近 中等
+ * 如果可以使用以下操作从一个字符串得到另一个字符串，则认为两个字符串 接近 ：
+ * <p>
+ * 操作 1：交换任意两个 现有 字符。
+ * 例如，abcde -> aecdb
+ * 操作 2：将一个 现有 字符的每次出现转换为另一个 现有 字符，并对另一个字符执行相同的操作。
+ * 例如，aacabb -> bbcbaa（所有 a 转化为 b ，而所有的 b 转换为 a ）
+ * 你可以根据需要对任意一个字符串多次使用这两种操作。
+ * <p>
+ * 给你两个字符串，word1 和 word2 。如果 word1 和 word2 接近 ，就返回 true ；否则，返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：word1 = "abc", word2 = "bca"
+ * 输出：true
+ * 解释：2 次操作从 word1 获得 word2 。
+ * 执行操作 1："abc" -> "acb"
+ * 执行操作 1："acb" -> "bca"
+ * 示例 2：
+ * <p>
+ * 输入：word1 = "a", word2 = "aa"
+ * 输出：false
+ * 解释：不管执行多少次操作，都无法从 word1 得到 word2 ，反之亦然。
+ * 示例 3：
+ * <p>
+ * 输入：word1 = "cabbba", word2 = "abbccc"
+ * 输出：true
+ * 解释：3 次操作从 word1 获得 word2 。
+ * 执行操作 1："cabbba" -> "caabbb"
+ * 执行操作 2："caabbb" -> "baaccc"
+ * 执行操作 2："baaccc" -> "abbccc"
+ * 提示：
+ * <p>
+ * 1 <= word1.length, word2.length <= 105
+ * word1 和 word2 仅包含小写英文字母
+ */
+public class Solution1657 {
+    public boolean closeStrings(String word1, String word2) {
+        int[] count1 = count(word1);
+        int[] count2 = count(word2);
+
+        for (int i = 0; i < 26; i++) {
+            if ((count1[i] == 0 && count2[i] != 0) || (count1[i] != 0 && count2[i] == 0)) {
+                return false;
+            }
+        }
+
+        Arrays.sort(count1);
+        Arrays.sort(count2);
+
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] != count2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int[] count(String word) {
+        int[] result = new int[26];
+        char[] charArray = word.toCharArray();
+        for (char c : charArray) {
+            result[c - 'a']++;
+        }
+        return result;
+    }
+
+
+}
